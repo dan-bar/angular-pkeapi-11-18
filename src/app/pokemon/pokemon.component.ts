3170e0d5afa4b70aeca1cd57fcb98d4ae40cf5c7
dan-bar <barbara.bandeira.t0114615@edu.atec.pt>
2019-11-08 21:54:57 +0000

    2 files modified

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Aluno } from '../pokemon';
+import { PokemonApiService } from '.. pokemon-api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-pokemon',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
+  @Input() pokemon:Pokemon;
+  pokemonAsync:Pokemon;
+  idRota:number;
 
-  constructor() { }
+
+  constructor(private pokemonApiService:PokemonApiService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.getRoute();
+    this.getPokemon();
+  }
+
+  getPokemon() {
+    this.pokemonApiService.getPokemonById(this.idRota).subscribe(data => this.pokemonAsync = data.results[0]);
+  }
+
+  getRoute() {
+    this.route.paramMap.subscribe(params => this.idRota = +params.get('idPokemon'));
   }
 
 }
